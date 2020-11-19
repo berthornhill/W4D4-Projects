473ddfd09fa32f393460f0578fed5fe172a17925
berthornhill <64211468+berthornhill@users.noreply.github.com>
2020-11-19 17:16:47 -0500

    Continued, towers_of_hanoi

--- TDD/lib/towers_of_hanoi.rb
+++ TDD/lib/towers_of_hanoi.rb
@@ -1,7 +1,21 @@
 class TowersOfHanoi
 
+    attr_reader :board
+
     def initialize
+        @board = Array.new(3) {[]}
+        @board[0] = [3,2,1]
 
     end
 
+    def move
+        print " Please select a pile to move a disc"
+        start_pile = gets.chomp.to_i
+        raise "please enter valid number" if !start_pile.is_a?(Integer)
+        
+        print " Please select destination pile."
+        desti_pile = gets.chomp.to_i
+
+
+    end
 end
--- TDD/spec/towers_of_hanoi_spec.rb
+++ TDD/spec/towers_of_hanoi_spec.rb
@@ -3,10 +3,18 @@ require 'towers_of_hanoi'
 describe TowersOfHanoi do
 
     describe "#initialize" do
+        subject(:towers_of_hanoi) {TowersOfHanoi.new}
 
         it "initializes when TowersOfHanoi.new is called" do 
             expect { TowersOfHanoi.new }.not_to raise_error
         end
+
+        it "initializes a starting board" do 
+            expect(towers_of_hanoi.board).to eq([[3,2,1],[],[]])
+            p towers_of_hanoi.board
+        end
+        
+
 
         it "calls a start_game method after initialize"
 
@@ -23,7 +31,7 @@ describe TowersOfHanoi do
         it "instance of board is 2D-array where each array has an integer value"
 
         it "call game_loop method"
-
+        
     end
 
     
